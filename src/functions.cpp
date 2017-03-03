f268fbf7446b2cafc4b471aacd1cca5283b271b1
Brunel Axel <axel.brunel@epitech.eu>
2017-03-03 22:32:41 +0100

    vive les patates

--- src/functions.cpp
+++ src/functions.cpp
@@ -5,7 +5,7 @@
 // Login   <debrau_c@epitech.net>
 // 
 // Started on  Fri Feb 10 13:33:10 2017 Carl DEBRAUWERE
-// Last update Thu Mar  2 23:02:26 2017 debrau_c
+// Last update Fri Mar  3 22:30:38 2017 Brunel Axel
 //
 
 #include "functions.hpp"
@@ -21,6 +21,8 @@ void epurStr(std::string &s){
     s.replace(s.find("\t"), 1, " ");
   while (s.find("  ") != std::string::npos)
     s.replace(s.find("  "), 2, " ");
+  // while (s[0] && s[0] == ' ')
+  //   s.erase(0);
 }
 
 bool	sortComponentByNameSup(Component *a, Component *b){
